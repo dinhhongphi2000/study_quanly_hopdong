cd718613cc8f1bdc38c68754b3df8eed16824c48
Đinh Hồng Phi <dinhhongphi@gmail.com>
2018-03-14 18:12:39 +0700

    init project

---
+++ QLHopDong/Model/DatabaseConnection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHopDong.Model
+{
+    /// <summary>
+    /// Quản lý kết nối tới database
+    /// </summary>
+    public class DatabaseConnection : IDisposable
+    {
+        protected SqlConnection _connection = null;
+        private static string _connectionString = "";
+
+        public DatabaseConnection()
+        {
+            //Khởi tạo đối tượng chứa thông tin kết nối database
+            if (_connection == null)
+                _connection = new SqlConnection(_connectionString);
+        }
+
+        /// <summary>
+        /// Đóng kết nối và hủy đối tượng không sử dụng
+        /// </summary>
+        public void Dispose()
+        {
+            CloseConnection();
+            _connection.Dispose();
+        }
+
+        /// <summary>
+        /// Mở kết nối tới database để truy vấn dữ liệu
+        /// </summary>
+        public void OpenConnection()
+        {
+            if (_connection.State == System.Data.ConnectionState.Closed)
+            {
+                _connection.Open();
+            }
+        }
+
+        /// <summary>
+        /// Đóng kết nối nếu không còn object nào sử dụng
+        /// </summary>
+        public void CloseConnection()
+        {
+            if(_connection.State != System.Data.ConnectionState.Closed)
+            {
+                _connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Lấy kết nối database
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection GetConnection()
+        {
+            return _connection;
+        }
+
+    }
+}
---
+++ QLHopDong/Model/DbHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHopDong.Model
+{
+    public class DbHandler : DatabaseConnection
+    {
+        private SqlDataAdapter _adapter;
+
+        public DbHandler()
+        {
+            _adapter = new SqlDataAdapter();
+
+        }
+
+        public DataTable GetData(string query)
+        {
+            return null;
+        }
+    }
+}
